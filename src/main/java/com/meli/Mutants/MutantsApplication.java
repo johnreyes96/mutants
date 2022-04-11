0e040f5d7c7fc94d43deb3f0a41410db257c14e3
johnfreyes <jhonfer96@hotmail.com>
2022-04-11 15:37:33 -0500

    add project structure

---
+++ src/main/java/com/meli/Mutants/MutantsApplication.java
@@ -0,0 +1,13 @@
+package com.meli.Mutants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsApplication.class, args);
+	}
+
+}
