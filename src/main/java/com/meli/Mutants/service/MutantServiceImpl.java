006b108d47115c71225f9b2be9cba61ddee18647
johnfreyes <jhonfer96@hotmail.com>
2022-04-12 10:31:39 -0500

    checks if a human is a mutant

--- src/main/java/com/meli/Mutants/service/MutantServiceImpl.java
+++ src/main/java/com/meli/Mutants/service/MutantServiceImpl.java
@@ -36,7 +36,49 @@ public class MutantServiceImpl implements IMutantService {
         return dnaMatrix;
     }
 
-    private boolean isMutant(char[][] dnaMatrix) {
+    /**
+     * Checks if a human is a mutant.
+     * @param dnaMatrix matrix {@code char[][]} NxN elements with characters ACGT to check
+     * @return {@code true} if there is more than one sequence of four identical letters,
+     * obliquely, horizontally, or vertically
+     */
+    protected boolean isMutant(char[][] dnaMatrix) {
+        for (char[] matrix : dnaMatrix) {
+            for (int indexColumn = 0; indexColumn < matrix.length - 3; indexColumn++) {
+                if (matrix[indexColumn] == matrix[indexColumn + 1]
+                        && matrix[indexColumn] == matrix[indexColumn + 2]
+                        && matrix[indexColumn] == matrix[indexColumn + 3]) {
+                    return true;
+                }
+            }
+        }
+        for (int indexRow = 0; indexRow < dnaMatrix.length - 3; indexRow++) {
+            for (int indexColumn = 0; indexColumn < dnaMatrix[indexRow].length; indexColumn++) {
+                if (dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 1][indexColumn]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 2][indexColumn]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 3][indexColumn]) {
+                    return true;
+                }
+            }
+        }
+        for (int indexRow = 0; indexRow < dnaMatrix.length - 3; indexRow++) {
+            for (int indexColumn = 0; indexColumn < dnaMatrix[indexRow].length - 3; indexColumn++) {
+                if (dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 1][indexColumn + 1]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 2][indexColumn + 2]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 3][indexColumn + 3]) {
+                    return true;
+                }
+            }
+        }
+        for (int indexRow = 0; indexRow < dnaMatrix.length - 3; indexRow++) {
+            for (int indexColumn = dnaMatrix[indexRow].length - 1; indexColumn >= dnaMatrix[indexRow].length - 3; indexColumn--) {
+                if (dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 1][indexColumn - 1]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 2][indexColumn - 2]
+                        && dnaMatrix[indexRow][indexColumn] == dnaMatrix[indexRow + 3][indexColumn - 3]) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
--- src/test/java/com/meli/Mutants/service/MutantServiceImplTest.java
+++ src/test/java/com/meli/Mutants/service/MutantServiceImplTest.java
@@ -70,6 +70,358 @@ public class MutantServiceImplTest {
         Assertions.assertEquals(expected[2][2], dnaMatrix[2][2]);
     }
 
+    @Test
+    public void isMutantWhenDNAMatrixIsEmptyThenMustReturnFalseTest() {
+        char[][] dnaMatrix = new char[0][];
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIsSmallerOf4x4ThenMustReturnFalseTest() {
+        char[][] dnaMatrix = new char[3][3];
+        dnaMatrix[0][0] = 'A';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs4x4AndHaveObliqueSequenceFourEqualLettersThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[4][4];
+        dnaMatrix[0][0] = 'A';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'A';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs4x4AndHaveOblique2SequenceFourEqualLettersThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[4][4];
+        dnaMatrix[0][0] = 'C';
+        dnaMatrix[0][1] = 'G';
+        dnaMatrix[0][2] = 'T';
+        dnaMatrix[0][3] = 'A';
+        dnaMatrix[1][0] = 'T';
+        dnaMatrix[1][1] = 'G';
+        dnaMatrix[1][2] = 'A';
+        dnaMatrix[1][3] = 'C';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'A';
+        dnaMatrix[2][2] = 'T';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'A';
+        dnaMatrix[3][2] = 'G';
+        dnaMatrix[3][3] = 'A';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs4x4AndHaveSequenceFourEqualLettersVerticalThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[4][4];
+        dnaMatrix[0][0] = 'G';
+        dnaMatrix[0][1] = 'C';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'A';
+        dnaMatrix[1][0] = 'G';
+        dnaMatrix[1][1] = 'T';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'C';
+        dnaMatrix[2][0] = 'A';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'G';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'A';
+        dnaMatrix[3][2] = 'G';
+        dnaMatrix[3][3] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs4x4AndHaveSequenceFourEqualLettersHorizontalThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[4][4];
+        dnaMatrix[0][0] = 'T';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'A';
+        dnaMatrix[0][3] = 'T';
+        dnaMatrix[1][0] = 'A';
+        dnaMatrix[1][1] = 'G';
+        dnaMatrix[1][2] = 'A';
+        dnaMatrix[1][3] = 'A';
+        dnaMatrix[2][0] = 'C';
+        dnaMatrix[2][1] = 'C';
+        dnaMatrix[2][2] = 'C';
+        dnaMatrix[2][3] = 'C';
+        dnaMatrix[3][0] = 'T';
+        dnaMatrix[3][1] = 'C';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'C';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs6x6AndHaveObliqueSequenceFourEqualLettersThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[6][6];
+        dnaMatrix[0][0] = 'A';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[0][4] = 'G';
+        dnaMatrix[0][5] = 'A';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[1][4] = 'G';
+        dnaMatrix[1][5] = 'C';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[2][4] = 'G';
+        dnaMatrix[2][5] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'A';
+        dnaMatrix[3][4] = 'C';
+        dnaMatrix[3][5] = 'G';
+        dnaMatrix[4][0] = 'C';
+        dnaMatrix[4][1] = 'T';
+        dnaMatrix[4][2] = 'C';
+        dnaMatrix[4][3] = 'C';
+        dnaMatrix[4][4] = 'T';
+        dnaMatrix[4][5] = 'A';
+        dnaMatrix[5][0] = 'T';
+        dnaMatrix[5][1] = 'C';
+        dnaMatrix[5][2] = 'A';
+        dnaMatrix[5][3] = 'C';
+        dnaMatrix[5][4] = 'T';
+        dnaMatrix[5][5] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs6x6AndHaveOblique2SequenceFourEqualLettersThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[6][6];
+        dnaMatrix[0][0] = 'A';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[0][4] = 'G';
+        dnaMatrix[0][5] = 'A';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[1][4] = 'G';
+        dnaMatrix[1][5] = 'A';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[2][4] = 'C';
+        dnaMatrix[2][5] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'C';
+        dnaMatrix[3][3] = 'C';
+        dnaMatrix[3][4] = 'G';
+        dnaMatrix[3][5] = 'G';
+        dnaMatrix[4][0] = 'C';
+        dnaMatrix[4][1] = 'T';
+        dnaMatrix[4][2] = 'C';
+        dnaMatrix[4][3] = 'C';
+        dnaMatrix[4][4] = 'T';
+        dnaMatrix[4][5] = 'A';
+        dnaMatrix[5][0] = 'T';
+        dnaMatrix[5][1] = 'C';
+        dnaMatrix[5][2] = 'A';
+        dnaMatrix[5][3] = 'C';
+        dnaMatrix[5][4] = 'T';
+        dnaMatrix[5][5] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs6x6AndHaveSequenceFourEqualLettersVerticalThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[6][6];
+        dnaMatrix[0][0] = 'T';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[0][4] = 'G';
+        dnaMatrix[0][5] = 'A';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[1][4] = 'G';
+        dnaMatrix[1][5] = 'C';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[2][4] = 'G';
+        dnaMatrix[2][5] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'A';
+        dnaMatrix[3][4] = 'G';
+        dnaMatrix[3][5] = 'G';
+        dnaMatrix[4][0] = 'A';
+        dnaMatrix[4][1] = 'C';
+        dnaMatrix[4][2] = 'C';
+        dnaMatrix[4][3] = 'C';
+        dnaMatrix[4][4] = 'T';
+        dnaMatrix[4][5] = 'A';
+        dnaMatrix[5][0] = 'T';
+        dnaMatrix[5][1] = 'C';
+        dnaMatrix[5][2] = 'A';
+        dnaMatrix[5][3] = 'C';
+        dnaMatrix[5][4] = 'T';
+        dnaMatrix[5][5] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs6x6AndHaveSequenceFourEqualLettersHorizontalThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[6][6];
+        dnaMatrix[0][0] = 'C';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[0][4] = 'A';
+        dnaMatrix[0][5] = 'A';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[1][4] = 'G';
+        dnaMatrix[1][5] = 'C';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[2][4] = 'G';
+        dnaMatrix[2][5] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'A';
+        dnaMatrix[3][4] = 'G';
+        dnaMatrix[3][5] = 'G';
+        dnaMatrix[4][0] = 'C';
+        dnaMatrix[4][1] = 'C';
+        dnaMatrix[4][2] = 'C';
+        dnaMatrix[4][3] = 'C';
+        dnaMatrix[4][4] = 'T';
+        dnaMatrix[4][5] = 'A';
+        dnaMatrix[5][0] = 'T';
+        dnaMatrix[5][1] = 'C';
+        dnaMatrix[5][2] = 'A';
+        dnaMatrix[5][3] = 'C';
+        dnaMatrix[5][4] = 'T';
+        dnaMatrix[5][5] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void isMutantWhenDNAMatrixIs6x6AndHave3SequencesFourEqualLettersThenMustReturnTrueTest() {
+        char[][] dnaMatrix = new char[6][6];
+        dnaMatrix[0][0] = 'A';
+        dnaMatrix[0][1] = 'T';
+        dnaMatrix[0][2] = 'G';
+        dnaMatrix[0][3] = 'C';
+        dnaMatrix[0][4] = 'G';
+        dnaMatrix[0][5] = 'A';
+        dnaMatrix[1][0] = 'C';
+        dnaMatrix[1][1] = 'A';
+        dnaMatrix[1][2] = 'G';
+        dnaMatrix[1][3] = 'T';
+        dnaMatrix[1][4] = 'G';
+        dnaMatrix[1][5] = 'C';
+        dnaMatrix[2][0] = 'T';
+        dnaMatrix[2][1] = 'T';
+        dnaMatrix[2][2] = 'A';
+        dnaMatrix[2][3] = 'T';
+        dnaMatrix[2][4] = 'G';
+        dnaMatrix[2][5] = 'T';
+        dnaMatrix[3][0] = 'A';
+        dnaMatrix[3][1] = 'G';
+        dnaMatrix[3][2] = 'A';
+        dnaMatrix[3][3] = 'A';
+        dnaMatrix[3][4] = 'G';
+        dnaMatrix[3][5] = 'G';
+        dnaMatrix[4][0] = 'C';
+        dnaMatrix[4][1] = 'C';
+        dnaMatrix[4][2] = 'C';
+        dnaMatrix[4][3] = 'C';
+        dnaMatrix[4][4] = 'T';
+        dnaMatrix[4][5] = 'A';
+        dnaMatrix[5][0] = 'T';
+        dnaMatrix[5][1] = 'C';
+        dnaMatrix[5][2] = 'A';
+        dnaMatrix[5][3] = 'C';
+        dnaMatrix[5][4] = 'T';
+        dnaMatrix[5][5] = 'G';
+
+        boolean result = mutantService.isMutant(dnaMatrix);
+
+        Assertions.assertTrue(result);
+    }
+
     @AfterEach
     void tearDown() {
         this.mutantService = null;
